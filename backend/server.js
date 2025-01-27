5552f5e9363d9131244075d7b288c5a21b72c058
Halyna Danylko <danilkogallina@gmail.com>
2025-01-27 14:13:03 +0200

    corrected path

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,22 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   {
+//     app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "dist", 'index.js' ))
-    })
-  }
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.js"));
+//     });
+//   }
+// }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
